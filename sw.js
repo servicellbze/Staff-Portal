354b39585b60920d3808e6006425c3433b742a70
ServiCell Belize <servicellbze@gmail.com>
2026-04-30 15:22:03 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Network-first strategy for fast security updates
 // ─────────────────────────────────────────────────────────────────────────────
 
-const CACHE_DATE = '2026-04-29k'; // ← change this to today's date on each deploy
+const CACHE_DATE = '2026-04-30a'; // ← change this to today's date on each deploy
 const CACHE_NAME = 'servicell-' + CACHE_DATE;
 const BASE = '/Staff-Portal';
 
